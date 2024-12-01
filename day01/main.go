37fbd23aa74f28b0084c3babd8700a62b30021f0
Apostolos Kiraleos <apostolos.kiraleos@theblueground.com>
2024-12-01 13:38:22 +0200

    day 1 part 2

--- day01/main.go
+++ day01/main.go
@@ -13,10 +13,44 @@ const DAY int = 1
 func main() {
 	lines := fetchInputLines(DAY)
 	
+	part1 := solvePart1(lines)
+	part2 := solvePart2(lines)
+
+	println("Part 1: ", part1)
+	println("Part 2: ", part2)
+}
+
+func solvePart1(lines []string) int {
 	firstColumn, secondColumn := splitToSortedColumns(lines)
 	totalDistance := calculateSumOfDistanceBetweenColumns(firstColumn, secondColumn)
+	return totalDistance
+}
 
-	println(totalDistance)
+func solvePart2(lines []string) int {
+	firstColumn, secondColumn := splitToSortedColumns(lines)
+	frequencies := calculateFrequencies(firstColumn, secondColumn)
+	similarityScore := calculateSimilarityScore(firstColumn, frequencies)
+	return similarityScore
+}
+
+func calculateSimilarityScore(firstColumn []int, frequencies map[int]int) int {
+	similarityScore := 0
+	for i := 0; i < len(firstColumn); i++ {
+		similarityScore += firstColumn[i] * frequencies[firstColumn[i]]
+	}
+	return similarityScore
+}
+
+func calculateFrequencies(firstColumn []int, secondColumn []int) map[int]int {
+	frequencies := make(map[int]int)
+	for i := 0; i < len(firstColumn); i++ {
+		for j := 0; j < len(secondColumn); j++ {
+			if firstColumn[i] == secondColumn[j] {
+				frequencies[firstColumn[i]]++
+			}
+		}
+	}
+	return frequencies
 }
 
 func fetchInputLines(day int) []string {
